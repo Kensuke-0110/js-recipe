a92d6a9dece210d7adab6eff23c42e80765404bc
Kensuke-0110 <kensuke.webex@gmail.com>
2023-04-29 20:16:03 +0900

    ゼミで配列を使ったメモアプリを作りました

---
+++ 04_ゼミ/session/week2/main.js
@@ -0,0 +1,18 @@
+// const addButton = document.getElementById("add-button")
+// let memoInput = document.getElementById("memo-input")
+// const memoContainer = document.getElementById("memo-container")
+
+// // 配列の定義
+// let memos=[]
+
+// addButton.onclick = function () {
+//   console.log(memoInput.value)
+//   //   メモに入力した値を表示するdivタグの生成
+//   const memo = document.createElement("div")
+//   memo.textContent = memoInput.value
+//   memoContainer.appendChild(memo)
+//   memoInput.value = null
+
+// // メモの内容を配列に保存
+
+// }
