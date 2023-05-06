a217f9088c776f02ec1b31adca739e2e9639a769
Kensuke-0110 <kensuke.webex@gmail.com>
2023-05-06 11:03:03 +0900

    Vueを使ってクイズアプリを作成しました

--- 02_レシピ/02_クイズ（１問）/main.js
+++ 02_レシピ/02_クイズ（１問）/main.js
@@ -1,8 +1,6 @@
 const quizText = document.getElementById("quiz-text")
 const quizImage = document.getElementById("quiz-image")
-const choice1 = document.getElementById("choice-1")
-const choice2 = document.getElementById("choice-2")
-const choice3 = document.getElementById("choice-3")
+const button = document.getElementById("button")
 const feedback = document.getElementById("feedback")
 
 // クイズの内容
@@ -23,21 +21,11 @@ const quiz = {
       text: "ガニメデ",
       feedback: "正解！ガニメデは、木星の第三惑星だよ！",
     },
+    {
+      text: "地球",
+      feedback: "残念！！！！！",
+    },
   ],
-}
-
-// quiz を画面に表示する関数
-const reloadQuiz = function () {
-  // 問題文を表示
-  quizText.textContent = "Q. " + quiz.text
-
-  // 画像を表示
-  quizImage.src = "./images/" + quiz.image
-
-  // 選択肢（ボタン）の中身を表示
-  choice1.textContent = quiz.choices[0].text
-  choice2.textContent = quiz.choices[1].text
-  choice3.textContent = quiz.choices[2].text
 }
 
 // choiceNumber番目の選択肢を選択
@@ -46,18 +34,25 @@ const choose = function (choiceNumber) {
   feedback.textContent = quiz.choices[choiceNumber].feedback
 }
 
-choice1.onclick = function () {
-  // 0 番目の選択肢を選択
-  choose(0)
-}
-choice2.onclick = function () {
-  // 1 番目の選択肢を選択
-  choose(1)
-}
-choice3.onclick = function () {
-  // 2 番目の選択肢を選択
-  choose(2)
+const reloadQuiz = function () {
+  quizText.textContent = "Q. " + quiz.text
+  quizImage.src = "./images/" + quiz.image
+
+  // ボタンの生成
+  const buttonMake = function () {
+    for (let i = 0; i < quiz.choices.length; i++) {
+      const btn = document.createElement("button")
+      // ボタンの内容
+      btn.innerHTML = quiz.choices[i].text
+      // イベントハンドラ定義
+      btn.onclick = function () {
+        choose(i)
+      }
+      // HTMLのbodyにbutton要素を追加
+      button.append(btn)
+    }
+  }
+  buttonMake()
 }
 
-// reloadQuiz関数 を実行して、クイズを画面に表示する
 reloadQuiz()
